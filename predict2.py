279b6206a8dde9a1a19113d5a47e822f50b7a66a
Yev Perevodchikov <evgeniyp@amazon.com>
2020-03-15 17:57:24 -0700

    fix the path for input file3

--- predict2.py
+++ predict2.py
@@ -628,7 +628,7 @@ if __name__== "__main__":
 
   if len(args) > 1:
       input_file_name = args[1]
-      if os.path.exists(input_file_path):
+      if os.path.exists(input_file_name):
           with open(input_file_name, "r", encoding='utf-8') as f_in:
               sentences_to_predict = f_in.readlines()
               sentences_to_predict = [line for line in sentences_to_predict if line.strip()]
